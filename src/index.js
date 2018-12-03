dba1132cc3ca94bd37b933a1c618ed678fb0d6d8
Aaron Zheng <aaronzheng3@gmail.com>
2018-12-02 21:59:55 -0500

    randomization

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import {drawPlayer, movement} from './player';
+import Obstacle from './obstacle'
 
 document.addEventListener("DOMContentLoaded", () => {
   let img = new Image();
@@ -11,6 +12,11 @@ document.addEventListener("DOMContentLoaded", () => {
 
   const canvas = document.getElementById('canvas');
   const ctx = canvas.getContext('2d');
+  const obs1 = new Obstacle(ctx,0,900);
+  const obs2 = new Obstacle(ctx,0,1300);
+  const obs3 = new Obstacle(ctx,0,1700);
+  const obs4 = new Obstacle(ctx,0,2100);
+
   canvas.tabIndex = 1;
   let playerX = 250
   let playerY = 40
@@ -21,7 +27,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
   function draw() {
     ctx.save();
-    if (offsetY + scrollY <= -2100) {
+    if (offsetY + scrollY <= -2500) {
       scrollY = 0;
       setTimeout(()=> changeDirection(),4000);
     } else if (offsetY + scrollY > 0) {
@@ -29,14 +35,9 @@ document.addEventListener("DOMContentLoaded", () => {
     }
     ctx.translate(0,scrollY);
     offsetY += scrollY;
-    // ctx.clearRect(-offsetX,-offsetY, 300,10000);
+    ctx.clearRect(-offsetX,-offsetY, 300,10000);
     playerBounds();
   }
-
-  function changeDirection() {
-    scrollY = 2.25;
-  }
-
 
   function playerBounds() {
     if (playerY - 3 < 1 - offsetY) {
@@ -46,76 +47,42 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   }
 
+  function changeDirection() {
+    scrollY = 2.25;
+  }
 
-  let imgIndex = [1,1,1,2,2,2,3,3,3,4,4,4,5,5,5,6,6,6];
+
+
+
+  let imgIndex = [1,1,1,1,1,1,
+                  2,2,2,2,2,2,
+                  3,3,3,3,3,3,
+                  4,4,4,4,4,4,
+                  5,5,5,5,5,5,
+                  6,6,6,6,6,6];
   let idx = 0;
   let frameCount = 0;
+
   function step() {
     ctx.beginPath();
     frameCount++;
-    if (frameCount < 2) {
+    if (frameCount < 1) {
       window.requestAnimationFrame(step);
       return;
     }
      frameCount = 0;
-     ctx.clearRect(-offsetX,-offsetY, canvas.width,canvas.height);
 
+    ctx.clearRect(-offsetX,-offsetY, canvas.width,canvas.height);
     drawSprite(imgIndex[idx]);
+    const ob1 = obs1.render();
+    const ob2 = obs2.render();
+    const ob3 = obs3.render();
+    const ob4 = obs4.render();
     let x = 0;
-    let y = 300;
-    let y2 = 700;
-    let y3 = 1100;
-    let y4 = 1700;
-    let y5 = 2000;
+    let y = 200;
 
-    // ctx.fillRect(x,y,150,300);
-    // ctx.fillRect(x+550,y,150,300);
-    // ctx.fillRect(x+150,y,100,50);
-    // ctx.fillRect(x+450,y,100,50);
-    // ctx.fillRect(x+150,y+250,100,50);
-    // ctx.fillRect(x+450,y+250,100,50);
-    // ctx.fillRect(x+328,y+100,40,100);
-    //
-    // ctx.fillRect(x,y2,80,250);  //left
-    // ctx.fillRect(x+600,y2,100,250); //right
-    // ctx.fillRect(x+80,y2,45,40); //left-notch1
-    // ctx.fillRect(x+80,y2+210,45,40); //left-notch2
-    // ctx.fillRect(x+200,y2,300,40); //mid-top
-    // ctx.fillRect(x+200,y2+210,300,40); //mid-bot
-    // ctx.fillRect(x+420,y2,80,250);//mid-column
-    //
-    // ctx.fillRect(x,y3,500,50); // left-bar
-    // ctx.fillRect(x+600,y3,100,50); // right-bar
-    // ctx.fillRect(x,y3+450,300,50); //left-bar2
-    // ctx.fillRect(x+400,y3+450,300,50); //right-bar2
-    // ctx.fillRect(x,y3+250,100,50); //left-bar3
-    // ctx.fillRect(x+200,y3+250,500,50);//right-bar3
-    //
-    // ctx.fillRect(x+195,y4,310,40); //mid-top
-    // ctx.fillRect(x,y4,80,250); //left
-    // ctx.fillRect(x+620,y4,80,250); //right
-    // ctx.fillRect(x+80,y4+200,190,50);//left-notch
-    // ctx.fillRect(x+430,y4+200,190,50);//right-notch
-    //
-    // ctx.fillRect(x,y5,70,40); //layer1-block1
-    // ctx.fillRect(x+130,y5,70,40); //layer1-block2
-    // ctx.fillRect(x+250,y5,70,40); //layer1-block3
-    // ctx.fillRect(x+630,y5,70,40); //layer1-block4
-    // ctx.fillRect(x+380,y5,70,40); //layer1-block5
-    // ctx.fillRect(x+500,y5,70,40); //layer1-block6
-    //
-    // ctx.fillRect(x+65,y5+150,70,40); //layer2-block1
-    // ctx.fillRect(x+195,y5+150,70,40); //layer2-block2
-    // ctx.fillRect(x+315,y5+150,70,40); //layer2-block3
-    // ctx.fillRect(x+445,y5+150,70,40); //layer2-block4
-    // ctx.fillRect(x+565,y5+150,70,40); //layer2-block5
-    //
-    // ctx.fillRect(x,y5+300,70,40); //layer3-block1
-    // ctx.fillRect(x+130,y5+300,70,40); //layer3-block2
-    // ctx.fillRect(x+250,y5+300,70,40); //layer3-block3
-    // ctx.fillRect(x+380,y5+300,70,40); //layer3-block4
-    // ctx.fillRect(x+500,y5+300,70,40); //layer3-block5
-    // ctx.fillRect(x+630,y5+300,70,40); //layer3-block6
+    ctx.fillRect(0,500,150,300);
+    ctx.fillRect(550,500,150,300);
     idx++
     if (idx > imgIndex.length - 1) {
       idx = 0;
--- src/obstacle.js
+++ src/obstacle.js
@@ -1,72 +1,79 @@
-const obstacleArray = [
-  [ // design 1
-    ctx.fillRect(x,y,150,300);
-    ctx.fillRect(x+550,y,150,300);
-    ctx.fillRect(x+150,y,100,50);
-    ctx.fillRect(x+450,y,100,50);
-    ctx.fillRect(x+150,y+250,100,50);
-    ctx.fillRect(x+450,y+250,100,50);
-    ctx.fillRect(x+328,y+100,40,100);
-  ],
-  [ //design 2
-    ctx.fillRect(x,y2,80,250);  //left
-    ctx.fillRect(x+600,y2,100,250); //right
-    ctx.fillRect(x+80,y2,45,40); //left-notch1
-    ctx.fillRect(x+80,y2+210,45,40); //left-notch2
-    ctx.fillRect(x+200,y2,300,40); //mid-top
-    ctx.fillRect(x+200,y2+210,300,40); //mid-bot
-    ctx.fillRect(x+420,y2,80,250);//mid-column
-  ],
-  [ //design 2 -flipped
-    ctx.fillRect(x,y,100,250);  //left
-    ctx.fillRect(x+620,y,80,250); //right
-    ctx.fillRect(x+600,y,45,40); //left-notch1
-    ctx.fillRect(x+600,y+210,45,40); //left-notch2
-    ctx.fillRect(x+200,y,310,40); //mid-top
-    ctx.fillRect(x+200,y+210,310,40); //mid-bot
-    ctx.fillRect(x+200,y,80,250);//mid-column
-  ],
-  [ //design 3
-    ctx.fillRect(x,y,500,50); // left-bar
-    ctx.fillRect(x+600,y,100,50); // right-bar
-    ctx.fillRect(x,y+300,300,50); //left-bar2
-    ctx.fillRect(x+400,y+300,300,50); //right-bar2
-    ctx.fillRect(x,y+150,100,50); //left-bar3
-    ctx.fillRect(x+200,y+150,500,50);//right-bar3
-  ],
-  [ //design 4
-    ctx.fillRect(x+195,y,310,40); //mid-top
-    ctx.fillRect(x,y,80,250); //left
-    ctx.fillRect(x+620,y,80,250); //right
-    ctx.fillRect(x+80,y+200,190,50);//left-notch
-    ctx.fillRect(x+430,y+200,190,50);//right-notch
-  ],
-  [ //design 5
-    ctx.fillRect(x,y,70,40); //layer1-block1
-    ctx.fillRect(x+130,y,70,40); //layer1-block2
-    ctx.fillRect(x+250,y,70,40); //layer1-block3
-    ctx.fillRect(x+630,y,70,40); //layer1-block4
-    ctx.fillRect(x+380,y,70,40); //layer1-block5
-    ctx.fillRect(x+500,y,70,40); //layer1-block6
-
-    ctx.fillRect(x+65,y+150,70,40); //layer2-block1
-    ctx.fillRect(x+195,y+150,70,40); //layer2-block2
-    ctx.fillRect(x+315,y+150,70,40); //layer2-block3
-    ctx.fillRect(x+445,y+150,70,40); //layer2-block4
-    ctx.fillRect(x+565,y+150,70,40); //layer2-block5
-
-    ctx.fillRect(x,y+300,70,40); //layer3-block1
-    ctx.fillRect(x+130,y+300,70,40); //layer3-block2
-    ctx.fillRect(x+250,y+300,70,40); //layer3-block3
-    ctx.fillRect(x+380,y+300,70,40); //layer3-block4
-    ctx.fillRect(x+500,y+300,70,40); //layer3-block5
-    ctx.fillRect(x+630,y+300,70,40); //layer3-block6
-  ],
-]
-
-
 class Obstacle {
-  constructor(ctx) {
 
+constructor(ctx,x,y) {
+  this.designs = [
+    [
+       {x:x,y:y,width:150,height:300},
+       {x:x+550,y:y,width:150,height:300},
+       {x:x+150,y:y,width:100,height:50},
+       {x:x+450,y:y,width:100,height:50},
+       {x:x+150,y:y+250,width:100,height:50},
+       {x:x+450,y:y+250,width:100,height:50},
+       {x:x+328,y:y+100,width:40,height:100},
+    ],
+    [ //design 2
+       {x:x,y:y,width:80,height:250},  //left
+       {x:x+600,y:y,width:100,height:250}, //right
+       {x:x+80,y:y,width:45,height:40}, //left-notch1
+       {x:x+80,y:y+210,width:45,height:40}, //left-notch2
+       {x:x+200,y:y,width:300,height:40}, //mid-top
+       {x:x+200,y:y+210,width:300,height:40}, //mid-bot
+       {x:x+420,y:y,width:80,height:250},//mid-column
+    ],
+    [ //design 2 -flipped
+       {x:x,y:y,width:100,height:250},  //left
+       {x:x+620,y:y,width:80,height:250}, //right
+       {x:x+600,y:y,width:45,height:40}, //left-notch1
+       {x:x+600,y:y+210,width:45,height:40}, //left-notch2
+       {x:x+200,y:y,width:310,height:40}, //mid-top
+       {x:x+200,y:y+210,width:310,height:40}, //mid-bot
+       {x:x+200,y:y,width:80,height:250},//mid-column
+    ],
+    [ //design 3
+       {x:x,y:y,width:500,height:50}, // left-bar
+       {x:x+600,y:y,width:100,height:50}, // right-bar
+       {x:x,y:y+300,width:300,height:50}, //left-bar2
+       {x:x+400,y:y+300,width:300,height:50}, //right-bar2
+       {x:x,y:y+150,width:100,height:50}, //left-bar3
+       {x:x+200,y:y+150,width:500,height:50},//right-bar3
+    ],
+    [ //design 4
+       {x:x+195,y:y,width:310,height:40}, //mid-top
+       {x:x,y:y,width:80,height:250}, //left
+       {x:x+620,y:y,width:80,height:250}, //right
+       {x:x+80,y:y+200,width:190,height:50},//left-notch
+       {x:x+430,y:y+200,width:190,height:50},//right-notch
+    ],
+    [ //design 5
+       {x:x,y:y,width:70,height:40}, //layer1-block1
+       {x:x+130,y:y,width:70,height:40}, //layer1-block2
+       {x:x+250,y:y,width:70,height:40}, //layer1-block3
+       {x:x+630,y:y,width:70,height:40}, //layer1-block4
+       {x:x+380,y:y,width:70,height:40}, //layer1-block5
+       {x:x+500,y:y,width:70,height:40}, //layer1-block6
+       {x:x+65,y:y+150,width:70,height:40}, //layer2-block1
+       {x:x+195,y:y+150,width:70,height:40}, //layer2-block2
+       {x:x+315,y:y+150,width:70,height:40}, //layer2-block3
+       {x:x+445,y:y+150,width:70,height:40}, //layer2-block4
+       {x:x+565,y:y+150,width:70,height:40}, //layer2-block5
+       {x:x,y:y+300,width:70,height:40}, //layer3-block1
+       {x:x+130,y:y+300,width:70,height:40}, //layer3-block2
+       {x:x+250,y:y+300,width:70,height:40}, //layer3-block3
+       {x:x+380,y:y+300,width:70,height:40}, //layer3-block4
+       {x:x+500,y:y+300,width:70,height:40}, //layer3-block5
+       {x:x+630,y:y+300,width:70,height:40}, //layer3-block6
+    ],
+  ];
+  let idx = Math.floor(Math.random()*this.designs.length);
+    this.obs = this.designs[idx];
+    this.ctx = ctx;
   }
+  render() {
+    for (let j = 0; j < this.obs.length; j++) {
+      this.ctx.fillRect(this.obs[j].x,this.obs[j].y,this.obs[j].width,this.obs[j].height);
+    }
+  }
+
 }
+
+export default Obstacle;
