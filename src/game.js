43fbf73a661e4fc6c5c1f460e28a45479c30e174
Aaron Zheng <aaronzheng3@gmail.com>
2018-12-12 15:17:40 -0500

    reformatted game.js

---
+++ src/game.js
@@ -0,0 +1,267 @@
+import Player from './player';
+import Obstacle from './obstacle'
+import Collectable from './collectable';
+import Treasure from './treasure';
+import Snowflakes from './snowflakes';
+class Game {
+  constructor() {
+
+     this.img = new Image();
+    this.img.src = "../FeatherFall/assets/ROfalcon.png";
+
+     this.canvas = document.getElementById('canvas');
+     this.offscreenCanvas = document.createElement('canvas');
+     this.context = this.offscreenCanvas.getContext('2d');
+    this.offscreenCanvas.width = "700";
+    this.offscreenCanvas.height = '6000';
+    this.canvas.tabIndex = 1;
+    this.offsetX = 0
+    this.offsetY = 0
+    this.scrollY = -.85
+     this.ctx = canvas.getContext('2d');
+     this.snow = new Snowflakes(this.canvas,this.ctx,this.offsetY);
+    this.snow.fillParticles();
+     this.obs1 = new Obstacle(this.context,this.ctx,0,400);
+     this.obs2 = new Obstacle(this.context,this.ctx,0,1000);
+     this.obs3 = new Obstacle(this.context,this.ctx,0,1600);
+     this.obs4 = new Obstacle(this.context,this.ctx,0,2200);
+     this.obs5 = new Obstacle(this.context,this.ctx,0,2800);
+     this.obs6 = new Obstacle(this.context,this.ctx,0,3400);
+     this.obs7 = new Obstacle(this.context,this.ctx,0,4200);
+     this.obs8 = new Obstacle(this.context,this.ctx,0,5000);
+     this.player = new Player(this.img,320,100,this.offsetY);
+     this.imgIndex = [1,1,1,1,1,1,
+                     2,2,2,2,2,2,
+                     3,3,3,3,3,3,
+                     4,4,4,4,4,4,
+                     5,5,5,5,5,5,
+                     6,6,6,6,6,6];
+     this.idx = 0;
+     this.frameCount = 0;
+     this.upArrow = false;
+     this.rightArrow = false;
+     this.downArrow = false;
+     this.leftArrow = false;
+     this.space = false;
+     this.draw = this.draw.bind(this);
+     this.init = this.init.bind(this);
+     this.fillOffscreen = this.fillOffscreen.bind(this);
+     this.step = this.step.bind(this);
+  }
+
+
+     draw() {
+      this.canvas.focus();
+      this.ctx.clearRect(-this.offsetX,-this.offsetY, 700,6000);
+      this.fillOffscreen();
+      this.ctx.save();
+      this.ctx.beginPath();
+      this.ctx.drawImage(this.offscreenCanvas,0,0);
+      this.moveCharacter();
+      if (this.offsetY + this.scrollY <= -5400) {
+        this.scrollY = 0;
+        setTimeout(()=> this.changeDirection(),4000);
+      } else if (this.offsetY + this.scrollY > 0) {
+        this.scrollY = 0;
+      }
+      this.ctx.translate(0,this.scrollY);
+      this.offsetY += this.scrollY;
+      this.ctx.closePath();
+      this.playerBounds();
+      this.updateStatus();
+      this.checkDeath();
+      window.requestAnimationFrame(this.draw);
+    }
+
+
+     fillOffscreen() {
+      this.obs1.borderWall(this.player);
+      let x = 0;
+      let y = 100;
+
+      this.obs1.render(this.player,this.offsetX,this.offsetY);
+      this.obs2.render(this.player,this.offsetX,this.offsetY);
+      this.obs3.render(this.player,this.offsetX,this.offsetY);
+      this.obs4.render(this.player,this.offsetX,this.offsetY);
+      this.obs5.render(this.player,this.offsetX,this.offsetY);
+      this.obs6.render(this.player,this.offsetX,this.offsetY);
+      this.obs7.render(this.player,this.offsetX,this.offsetY);
+      this.obs8.render(this.player,this.offsetX,this.offsetY);
+
+      return this.offscreenCanvas;
+    }
+
+     playerBounds() {
+      if (this.player.playerY - 3 < 1 - this.offsetY) {
+        this.player.playerY += 2;
+      } else if (this.player.playerY + 3 > this.canvas.height - this.offsetY - 40) {
+
+        this.player.playerY -= 4;
+      }
+    }
+
+     changeDirection() {
+      this.scrollY = 1.65;
+      // this.ctx.clearRect(-this.offsetX,-this.offsetY, 700,3000);
+    }
+
+
+
+
+
+
+     step() {
+      this.frameCount++;
+      if (this.frameCount < 1) {
+        // console.log(this.frameCount);
+        window.requestAnimationFrame(this.step);
+        return;
+      }
+      this.ctx.beginPath();
+       this.frameCount = 0;
+        this.snow.draw(this.offsetY);
+        this.player.drawSprite(this.ctx,this.imgIndex[this.idx],this.leftArrow,this.rightArrow);
+        this.ctx.closePath();
+
+        // this.ctx.clearRect(-this.offsetX,-this.offsetY, 700,3000);
+      let x = 0;
+      let y = 200;
+
+      this.idx++
+      if (this.idx > this.imgIndex.length - 1) {
+        this.idx = 0;
+      }
+      window.requestAnimationFrame(this.step);
+    }
+
+
+     init() {
+      window.requestAnimationFrame(this.step);
+    }
+
+    // let gameKeys = [];
+    // movement(playerX,playerY,this.offsetY);
+    checkKeyUp(e) {
+
+        // gameKeys[e.keyCode] = false;
+        if (e.keyCode == 38) {
+          this.upArrow = false;
+        }
+        if (e.keyCode == 37) {
+          this.leftArrow = false;
+        }
+        if (e.keyCode == 39) {
+          this.rightArrow = false;
+        }
+        if (e.keyCode == 40) {
+          this.downArrow = false;
+        }
+        // if (e.keyCode == 32) {
+        //   this.space = true;
+        // }
+    }
+    checkKeyDown(e) {
+
+
+      // gameKeys = gameKeys || [];
+      // gameKeys[e.keyCode] = true;
+      if (e.keyCode == 38) {
+        this.upArrow = true;
+      }
+      if (e.keyCode == 37) {
+        this.leftArrow = true;
+      }
+      if (e.keyCode == 39) {
+        this.rightArrow = true;
+      }
+      if (e.keyCode == 40) {
+        this.downArrow = true;
+      }
+      if (e.keyCode == 32) {
+        this.space = true;
+      }
+   }
+      moveCharacter() {
+       if (this.rightArrow) {
+
+         if (this.player.playerX + 3 > 660) {
+           this.player.playerX -= 8;
+         } else {
+           this.player.playerX += 2
+           if (this.space && this.player.dashReady > 0) {
+             this.player.dashReady -= 1
+             let interval =  setInterval(() => this.player.dashRight(),20);
+             setTimeout(() => this.player.stopDash(interval),360);
+           }
+           this.space = false
+         }
+
+       }
+       if (this.leftArrow) {
+
+          if (this.player.playerX - 3 < 0) {
+            this.player.playerX += 8;
+          } else {
+            this.player.playerX -= 2;
+            if (this.space && this.player.dashReady > 0) {
+              this.player.dashReady -= 1
+              let interval =  setInterval(() => this.player.dashLeft(),20);
+              setTimeout(() => this.player.stopDash(interval),360);
+            }
+            this.space = false
+          }
+       }
+        if (this.upArrow) {
+         if(this.player.playerY - 3 < 1 - this.offsetY) {
+           this.player.playerY += 8;
+         } else {
+           this.player.playerY -= 2.25;
+           if (this.space && this.player.dashReady > 0) {
+             this.player.dashReady -= 1
+             let interval =  setInterval(() => this.player.dashUp(),20);
+             setTimeout(() => this.player.stopDash(interval),360);
+           }
+           this.space = false
+         }
+       }
+        if (this.downArrow) {
+           if(this.player.playerY + 3 > this.canvas.height - this.offsetY - 50) {
+             this.player.playerY -= 8;
+           } else {
+             this.player.playerY += 1.75;
+             if (this.space && this.player.dashReady > 0) {
+               this.player.dashReady -= 1
+               let interval =  setInterval(() => this.player.dashDown(),20);
+               setTimeout(() => this.player.stopDash(interval),360);
+             }
+             this.space = false
+           }
+         }
+     }
+
+
+
+      //  draw();
+      // window.requestAnimationFrame(draw);
+
+       updateStatus() {
+        this.player.tallyScore();
+        document.getElementById('score').innerHTML= `Score: ${this.player.score}`;
+        document.getElementById('dash').innerHTML= `Dashes: ${this.player.dashReady}`;
+        document.getElementById('coins').innerHTML= `Crystals Collected: ${this.player.coins}`;
+        document.getElementById('hp').innerHTML= `Health: ${this.player.health}`;
+        document.getElementById('jewels').innerHTML= `Clusters: ${this.player.jewels}`
+      }
+
+       checkDeath() {
+        if (this.player.health <= 0 || this.player.outOfBounds(this.canvas,this.offsetY)) {
+          this.ctx.clearRect(-this.offsetX,-this.offsetY,700,10000);
+          this.context.clearRect(-this.offsetX,-this.offsetY,700,10000);
+          document.location.reload();
+          // game();
+        } else {
+          return;
+        }
+      }
+    }
+  export default Game;
--- src/index.js
+++ src/index.js
@@ -3,263 +3,27 @@ import Obstacle from './obstacle'
 import Collectable from './collectable';
 import Treasure from './treasure';
 import Snowflakes from './snowflakes';
-
+import Game from './game';
 document.addEventListener("DOMContentLoaded", () => {
-
-  function game () {
-
-
-  const img = new Image();
-  img.src = "../FeatherFall/assets/ROfalcon.png";
-  img.onload = function() {
-    init();
-  };
-  const canvas = document.getElementById('canvas');
-  const offscreenCanvas = document.createElement('canvas');
-  const context = offscreenCanvas.getContext('2d');
-  offscreenCanvas.width = "700";
-  offscreenCanvas.height = '6000';
-  canvas.tabIndex = 1;
-  let offsetX = 0
-  let offsetY = 0
-  let scrollY = -.85
-  const ctx = canvas.getContext('2d');
-  const snow = new Snowflakes(canvas,ctx,offsetY);
-  snow.fillParticles();
-  const obs1 = new Obstacle(context,ctx,0,400);
-  const obs2 = new Obstacle(context,ctx,0,1000);
-  const obs3 = new Obstacle(context,ctx,0,1600);
-  const obs4 = new Obstacle(context,ctx,0,2200);
-  const obs5 = new Obstacle(context,ctx,0,2800);
-  const obs6 = new Obstacle(context,ctx,0,3400);
-  const obs7 = new Obstacle(context,ctx,0,4200);
-  const obs8 = new Obstacle(context,ctx,0,5000);
-
-
-  const player = new Player(img,320,100,offsetY);
-
-  function draw() {
-    canvas.focus();
-    ctx.clearRect(-offsetX,-offsetY, 700,6000);
-    fillOffscreen();
-    ctx.save();
-    ctx.beginPath();
-    ctx.drawImage(offscreenCanvas,0,0);
-    moveCharacter();
-    if (offsetY + scrollY <= -5400) {
-      scrollY = 0;
-      setTimeout(()=> changeDirection(),4000);
-    } else if (offsetY + scrollY > 0) {
-      scrollY = 0;
-    }
-    ctx.translate(0,scrollY);
-    offsetY += scrollY;
-    ctx.closePath();
-    playerBounds();
-    updateStatus();
-    checkDeath();
-    window.requestAnimationFrame(draw);
+  const game = new Game();
+  function setup() {
+    document.getElementById("game-start").classList.add("hide");
+    game.draw();
+    game.img.onload = function() {
+      game.init();
+    };
   }
 
+  game.canvas.addEventListener('keydown',(e) => {
+    game.checkKeyDown(e);
+  },false);
 
-  function fillOffscreen() {
-    obs1.borderWall(player);
-    let x = 0;
-    let y = 100;
+  game.canvas.addEventListener('keyup',(e) => {
+    game.checkKeyUp(e);
+  },false);
 
-    obs1.render(player,offsetX,offsetY);
-    obs2.render(player,offsetX,offsetY);
-    obs3.render(player,offsetX,offsetY);
-    obs4.render(player,offsetX,offsetY);
-    obs5.render(player,offsetX,offsetY);
-    obs6.render(player,offsetX,offsetY);
-    obs7.render(player,offsetX,offsetY);
-    obs8.render(player,offsetX,offsetY);
-
-    return offscreenCanvas;
-  }
-
-  function playerBounds() {
-    if (player.playerY - 3 < 1 - offsetY) {
-      player.playerY += 2;
-    } else if (player.playerY + 3 > canvas.height - offsetY - 40) {
-
-      player.playerY -= 4;
-    }
-  }
-
-  function changeDirection() {
-    scrollY = 1.65;
-    // ctx.clearRect(-offsetX,-offsetY, 700,3000);
-  }
+  document.getElementById("start-btn").addEventListener("click",setup());
 
 
 
-
-  let imgIndex = [1,1,1,1,1,1,
-                  2,2,2,2,2,2,
-                  3,3,3,3,3,3,
-                  4,4,4,4,4,4,
-                  5,5,5,5,5,5,
-                  6,6,6,6,6,6];
-  let idx = 0;
-  let frameCount = 0;
-
-  function step() {
-    frameCount++;
-    if (frameCount < 1) {
-      // console.log(frameCount);
-      window.requestAnimationFrame(step);
-      return;
-    }
-    ctx.beginPath();
-     frameCount = 0;
-      snow.draw(offsetY);
-      player.drawSprite(ctx,imgIndex[idx],leftArrow,rightArrow);
-      ctx.closePath();
-
-      // ctx.clearRect(-offsetX,-offsetY, 700,3000);
-    let x = 0;
-    let y = 200;
-
-    idx++
-    if (idx > imgIndex.length - 1) {
-      idx = 0;
-    }
-    window.requestAnimationFrame(step);
-  }
-
-
-  function init() {
-    window.requestAnimationFrame(step);
-  }
-
-  // let gameKeys = [];
-  let upArrow = false;
-  let rightArrow = false;
-  let downArrow = false;
-  let leftArrow = false;
-  let space = false;
-  // movement(playerX,playerY,offsetY);
-  canvas.addEventListener('keyup', function(e) {
-    // gameKeys[e.keyCode] = false;
-    if (e.keyCode == 38) {
-      upArrow = false;
-    }
-    if (e.keyCode == 37) {
-      leftArrow = false;
-    }
-    if (e.keyCode == 39) {
-      rightArrow = false;
-    }
-    if (e.keyCode == 40) {
-      downArrow = false;
-    }
-    // if (e.keyCode == 32) {
-    //   space = true;
-    // }
-
-  })
-  canvas.addEventListener('keydown', function(e) {
-    // gameKeys = gameKeys || [];
-    // gameKeys[e.keyCode] = true;
-    if (e.keyCode == 38) {
-      upArrow = true;
-    }
-    if (e.keyCode == 37) {
-      leftArrow = true;
-    }
-    if (e.keyCode == 39) {
-      rightArrow = true;
-    }
-    if (e.keyCode == 40) {
-      downArrow = true;
-    }
-    if (e.keyCode == 32) {
-      space = true;
-    }
-   }, false);
-   function moveCharacter() {
-     if (rightArrow) {
-       if (player.playerX + 3 > 660) {
-         player.playerX -= 5;
-       } else {
-         player.playerX += 2
-         if (space && player.dashReady > 0) {
-           player.dashReady -= 1
-           let interval =  setInterval(() => player.dashRight(),20);
-           setTimeout(() => player.stopDash(interval),360);
-         }
-         space = false
-       }
-
-     }
-     if (leftArrow) {
-
-        if (player.playerX - 3 < 0) {
-          player.playerX += 5;
-        } else {
-          player.playerX -= 2;
-          if (space && player.dashReady > 0) {
-            player.dashReady -= 1
-            let interval =  setInterval(() => player.dashLeft(),20);
-            setTimeout(() => player.stopDash(interval),360);
-          }
-          space = false
-        }
-     }
-      if (upArrow) {
-       if(player.playerY - 3 < 1 - offsetY) {
-         player.playerY += 5;
-       } else {
-         player.playerY -= 2.25;
-         if (space && player.dashReady > 0) {
-           player.dashReady -= 1
-           let interval =  setInterval(() => player.dashUp(),20);
-           setTimeout(() => player.stopDash(interval),360);
-         }
-         space = false
-       }
-     }
-      if (downArrow) {
-         if(player.playerY + 3 > canvas.height - offsetY - 50) {
-           player.playerY -= 5;
-         } else {
-           player.playerY += 1.75;
-           if (space && player.dashReady > 0) {
-             player.dashReady -= 1
-             let interval =  setInterval(() => player.dashDown(),20);
-             setTimeout(() => player.stopDash(interval),360);
-           }
-           space = false
-         }
-       }
-   }
-
-
-
-     draw();
-    window.requestAnimationFrame(draw);
-
-    function updateStatus() {
-      player.tallyScore();
-      document.getElementById('score').innerHTML= `Score: ${player.score}`;
-      document.getElementById('dash').innerHTML= `Dashes: ${player.dashReady}`;
-      document.getElementById('coins').innerHTML= `Crystals Collected: ${player.coins}`;
-      document.getElementById('hp').innerHTML= `Health: ${player.health}`;
-      document.getElementById('jewels').innerHTML= `Clusters: ${player.jewels}`
-    }
-
-    function checkDeath() {
-      if (player.health <= 0 || player.outOfBounds(canvas,offsetY)) {
-        ctx.clearRect(-offsetX,-offsetY,700,10000);
-        context.clearRect(-offsetX,-offsetY,700,10000);
-        document.location.reload();
-        // game();
-      } else {
-        return;
-      }
-    }
-  }
-  game();
 });
